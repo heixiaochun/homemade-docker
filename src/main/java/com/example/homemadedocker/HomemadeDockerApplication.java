a85c19e66e4a2bea5260f800f9538b1c7990533d
heixiaochun <heixiaochun2046@gmail.com>
2020-02-22 18:34:12 +0800

    init

---
+++ src/main/java/com/example/homemadedocker/HomemadeDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.homemadedocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomemadeDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomemadeDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/homemadedocker/HomemadeDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homemadedocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomemadeDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
